9885b5c8fe09e5afcb9d2b7779f5ff24bc8a8ce0
Gustavo-AB <g.alvarez.b12@gmail.com>
2022-08-12 11:22:31 -0300

    menu responsivo adicionado

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+const btnMenu = document.querySelector('button#menu-button') ;
+
+function toggleMenu(){
+    const nav = document.querySelector('nav#nav');
+    nav.classList.toggle('active')
+}
+
+btnMenu.addEventListener('click', toggleMenu);

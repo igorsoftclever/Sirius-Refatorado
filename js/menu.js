db62a4765290258212e818410863ba0f36815307
Gabriel Toth Gonçalves <60109701+GabrielToth@users.noreply.github.com>
2022-10-21 17:56:33 -0300

    Menu update
    Menu responsivo.
    
    [HOTFIXES - ou fichas quentes]
    Remoção de bugs de responsividade (quase todos)

--- js/menu.js
+++ js/menu.js
@@ -1,3 +1,40 @@
-function menuAnimation() {
-    
+const btn = document.querySelector('.menu-button');
+const menu = document.querySelector('.menu');
+
+
+btn.addEventListener('click', function () {
+
+    if (menu.style.display === 'grid') {
+        menu.style.display = 'none';
+    } else {
+        menu.style.display = 'grid';
+    }
+});
+
+
+const controles = document.querySelectorAll(".hamburger-menu");
+
+for (let elements of controles) {
+    elements.onclick = function (e) {
+        e.preventDefault();
+        document.querySelector(".hamburger-menu").classList.toggle("hamburger-menu-open");
+    }
 }
+
+window.addEventListener('resize', function () {
+    //var altura = window.innerHeight;
+    var win_width = window.innerWidth;
+
+    if (win_width > 790) {
+        menu.style.display = 'grid';
+        document.querySelector(".hamburger-menu").classList.remove("hamburger-menu-open");
+    } else {
+        menu.style.display === 'none'
+        document.querySelector(".hamburger-menu").classList.remove("hamburger-menu-open");
+    }
+
+    /* 
+        Se tela menor q 790 e menu NÃO aparecer Remover hamburger-menu-open
+        Se tela menor q 790 e menu aparecer adicionar hamburger-menu
+    */
+});
